724e27abcfaa4f02c5ce08fc91dfabb8afc3dad5
June Leoman Lapera <leomanlapera@gmail.com>
2025-11-27 22:11:15 +0800

    player movement

--- player/player.gd
+++ player/player.gd
@@ -10,3 +10,16 @@ func _unhandled_input(event: InputEvent) -> void:
 		%Camera3D.rotation_degrees.x = clamp(%Camera3D.rotation_degrees.x, -60.0, 60.0)
 	elif event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _physics_process(_delta: float) -> void:
+	const SPEED: float = 5.5
+	
+	var input_direction_2D = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var input_direction_3D = Vector3(input_direction_2D.x, 0.0, input_direction_2D.y)
+	
+	var direction = transform.basis * input_direction_3D
+	
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+	
+	move_and_slide()
