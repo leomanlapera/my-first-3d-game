4eca57f75851cbcbe06d2f81e529b02fa236b6d7
June Leoman Lapera <leomanlapera@gmail.com>
2025-11-29 23:03:05 +0800

    bullet

---
+++ player/projectile/bullet_3d.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+const SPEED: float = 5.0
+const RANGE: float = 40.0
+
+var travelled_distance = 0.0
+
+func _physics_process(delta: float) -> void:
+	position += -transform.basis.z * SPEED * delta
+	travelled_distance += SPEED * delta
+
+	if travelled_distance > RANGE:
+		queue_free()
